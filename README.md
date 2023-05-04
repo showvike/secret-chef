cc8af0c6b599e70ded31010fd0c6b6e8dda0bb21
showvike <showvike@gmail.com>
2023-05-04 23:06:17 +0600

    read me file added

---
+++ README.md
@@ -0,0 +1,15 @@
+# List of best features of this project:
+
+1. Can Be Login With Email And Password
+2. Also Can Be Login With Google And GitHub
+3. Can Be Register Account With Email And Password
+4. Have A Feature Of Profile Picture
+5. Have A Feature Of Display Name
+6. A Blog Page For Blogging
+7. Chefs To Know
+8. Viewing Their Recipes
+9. Loading Spinner For Data Load
+10. Active Link For Routes
+11. Responsive For Both Mobile And PC
+
+**_Live Link:_** https://secret-chef-30e53.web.app/
--- src/components/Share/Header/Header.jsx
+++ src/components/Share/Header/Header.jsx
@@ -44,11 +44,18 @@ const Header = () => {
           </li>
           <li>
             {!user ? (
-              <Link to="/login">
-                <button className="bg-white text-black px-4 py-2 rounded">
-                  Login
-                </button>
-              </Link>
+              <NavLink
+                to="/login"
+                className={({ isActive, isPending }) =>
+                  isActive
+                    ? "text-green-500"
+                    : isPending
+                    ? "text-yellow-500"
+                    : "text-black"
+                }
+              >
+                <button className="bg-white px-4 py-2 rounded">Login</button>
+              </NavLink>
             ) : (
               <div>
                 <button onClick={handleLogOut}>
